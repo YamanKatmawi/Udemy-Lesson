121e924ede43836e8541041cad5d3582a3854b72
YamanKatmawi <yamankatmawi@gmx.de>
2020-08-20 13:56:41 +0200

    Loop-ex

---
+++ js/20-8/index.js
@@ -0,0 +1,22 @@
+// //alert
+// alert("hello there");
+// ///prompt
+// prompt("what is your name");
+// let userName = prompt("How old are you");
+let counter = -10;
+while (counter < 20) {
+  console.log(counter);
+  counter++;
+}
+console.log("printing all nambers between -10 and 19");
+///////
+let counterNumber = 10;
+while (counterNumber <= 40) {
+  counterNumber++;
+  console.log(counterNumber);
+}
+////////
+let Number = 10;
+for (let i = 0; i <= 40; i++) {
+  console.log(i);
+}
