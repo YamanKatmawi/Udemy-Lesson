8388333d3c51ca5f5db4505f197be029a3541578
YamanKatmawi <yamankatmawi@gmx.de>
2020-08-27 13:05:07 +0200

    Object

---
+++ js/27-8/index.js
@@ -0,0 +1,11 @@
+//Objects//
+let person = {
+  name: "yaman",
+  age: 32,
+  city: "Berlin",
+};
+console.log(person["city"]);
+console.log(person.age);
+person.city = "london";
+console.log(person.city);
+/////
