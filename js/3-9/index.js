8e2ff62e5eb8b04ebcf9fc4bfac5720c90beaba0
YamanKatmawi <yamankatmawi@gmx.de>
2020-09-03 11:49:48 +0200

    JS Stalker Solution

---
+++ js/3-9/index.js
@@ -0,0 +1,6 @@
+let firstName = prompt("what is you first Name?");
+let lastName = prompt("what is your last Name?");
+let age = prompt("whats is your age?");
+let fullName = firstName + " " + lastName;
+console.log("your full name is " + fullName);
+console.log("your are " + age + " years old");
