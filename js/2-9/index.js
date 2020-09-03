814de4399656811abc4f7e2be5ed796d834e8e81
YamanKatmawi <yamankatmawi@gmx.de>
2020-09-03 11:23:14 +0200

    Comparison with Arrays AND Objects

---
+++ js/2-9/index.js
@@ -0,0 +1,19 @@
+//Array//
+let dogs = ["Rusty", "lucky", "bubba"];
+console.log(dogs);
+console.log(dogs[1]);
+console.log(dogs.push("wyatt"));
+console.log(dogs);
+dogs[1] = "lucy";
+console.log(dogs);
+//objects//
+let dog = {
+  name: "bubba",
+  breed: "lab",
+};
+console.log(dog);
+console.log(dog.name);
+dog.age = 6;
+console.log(dog);
+dog.breed = "black lab";
+console.log(dog);
