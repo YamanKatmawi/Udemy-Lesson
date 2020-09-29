63ce461023141606c13b3302fd8032250cfd4acc
YamanKatmawi <yamankatmawi@gmx.de>
2020-09-29 15:09:52 +0200

    lifeCycle hooks

---
+++ React/28-9/new-app/src/ components /conditions.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function List(props) {
+  const age = 21;
+  return (
+    <div>
+      {age > 12 ? (
+        <div>You are older than 12.</div>
+      ) : (
+        <div>you are smaller than 12</div>
+      )}
+      {age > 20 ? <div>you are older than 20.</div> : null}
+    </div>
+  );
+}
+
+export default List;
--- React/28-9/new-app/src/App.js
+++ React/28-9/new-app/src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Welcome from "./ components /welcome";
 import Hello from "./ components /hello";
-import Lists from "./ components /lists";
+import List from "./ components /lists";
+import Conditions from "./ components /conditions";
+import LifeCycleExample from "./ components /LifeCycleExample";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       welcome yaman React
+      <hr />
       <Welcome name="yaman" lastName="katmawi">
         Description
       </Welcome>
+      <hr />
       <Hello name="yaman" />
-      <Lists />
+      <hr />
+      <List />
+      <hr />
+      <Conditions />
+      <hr />
+      <LifeCycleExample />
     </div>
   );
 }
