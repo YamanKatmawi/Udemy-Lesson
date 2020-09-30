eb51666155bfba636d4844da0c957902dcc6cb29
YamanKatmawi <yamankatmawi@gmx.de>
2020-09-30 08:47:52 +0200

    Conditions and loop-print Array of Objects

--- React/29-9/learn-app/src/App.js
+++ React/29-9/learn-app/src/App.js
@@ -6,12 +6,31 @@ class App extends Component {
   render() {
     let age = 10;
     let ageTemplate = "";
+    let users = [
+      { id: 1, name: "yaman", age: 32 },
+      { id: 2, name: "Ahmad", age: 27 },
+      { id: 3, name: "Homam", age: 20 },
+    ];
     if (age > 5) {
       ageTemplate = <h1>you are older than 5</h1>;
     } else {
       ageTemplate = <p>you are smaller than 5</p>;
     }
-    return <div className="App">{ageTemplate}</div>;
+    return (
+      <div className="App">
+        {ageTemplate}
+        <ul>
+          {users.map((
+            user,
+            index //we used index for the number automatic
+          ) => (
+            <li key={user.id}>
+              {index}-{user.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
