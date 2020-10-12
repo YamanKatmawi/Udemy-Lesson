8a356765d860d1b5171093681e1bacc6a05b7baa
YamanKatmawi <yamankatmawi@gmx.de>
2020-10-12 09:38:01 +0200

    Forms

---
+++ React/12-10/one-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    value: "Default",
+  };
+
+  setValue = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <input value={this.state.value} onChange={this.setValue} />
+      </div>
+    );
+  }
+}
+
+export default App;
