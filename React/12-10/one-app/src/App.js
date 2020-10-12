c8a08f7b26e6ceaec41601e2a69f639455eeca29
YamanKatmawi <yamankatmawi@gmx.de>
2020-10-12 15:42:12 +0200

    Forms-Handling Multiple Inputs

--- React/12-10/one-app/src/App.js
+++ React/12-10/one-app/src/App.js
@@ -11,35 +11,33 @@ class App extends Component {
   };
 
   setValue = (event) => {
-    this.setState({
-      value: event.target.value,
-    });
-  };
-  setSelectValue = (event) => {
-    this.setState({
-      setSelectValue: event.target.value,
-    });
-  };
-  setCheckboxValue = (event) => {
-    this.setState({
-      checked: event.target.checked,
-    });
-  };
+    const name = event.target.name;
+    const type = event.target.type;
+    let value = "";
+    if (type == "checkbox") value = event.target.checked;
+    else value = event.target.value;
 
-  changeRadio = (event) => {
     this.setState({
-      radioValue: event.target.value,
+      [name]: value,
     });
   };
 
   render() {
     return (
       <div className="App">
-        <input value={this.state.value} onChange={this.setValue} />
+        <input name="value" value={this.state.value} onChange={this.setValue} />
         <hr />
-        <textarea value={this.state.value} onChange={this.setValue} />
+        <textarea
+          name="value"
+          value={this.state.value}
+          onChange={this.setValue}
+        />
         <hr />
-        <select value={this.state.selectValue} onChange={this.setSelectValue}>
+        <select
+          name="selectValue"
+          value={this.state.selectValue}
+          onChange={this.setValue}
+        >
           <option value="1">one</option>
           <option value="2">two</option>
         </select>
@@ -47,20 +45,23 @@ class App extends Component {
         <label>
           Change checkbox
           <input
+            name="checked"
             type="checkbox"
             checked={this.state.checked}
-            onChange={this.setCheckboxValue}
+            onChange={this.setValue}
           />
         </label>
         <hr />
-        <div onChange={this.changeRadio}>
+        <div onChange={this.setValue}>
           <input
+            name="radioValue"
             type="radio"
             value="1"
             checked={this.state.radioValue == "1"}
           />
           one
           <input
+            name="radioValue"
             type="radio"
             value="2"
             checked={this.state.radioValue == "2"}
