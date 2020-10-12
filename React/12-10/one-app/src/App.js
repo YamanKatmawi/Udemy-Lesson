5486075b43180e2dc25b6bd4ddd732cc227e1f30
YamanKatmawi <yamankatmawi@gmx.de>
2020-10-12 10:17:17 +0200

    checkbox

--- React/12-10/one-app/src/App.js
+++ React/12-10/one-app/src/App.js
@@ -5,6 +5,8 @@ import "./App.css";
 class App extends Component {
   state = {
     value: "Default",
+    selectValue: "2",
+    checked: true,
   };
 
   setValue = (event) => {
@@ -12,10 +14,36 @@ class App extends Component {
       value: event.target.value,
     });
   };
+  setSelectValue = (event) => {
+    this.setState({
+      setSelectValue: event.target.value,
+    });
+  };
+  setCheckboxValue = (event) => {
+    this.setState({
+      checked: event.target.checked,
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <input value={this.state.value} onChange={this.setValue} />
+        <hr />
+
+        <textarea value={this.state.value} onChange={this.setValue} />
+        <hr />
+        <select value={this.state.selectValue} onChange={this.setSelectValue}>
+          <option value="1">one</option>
+          <option value="2">two</option>
+        </select>
+        <hr />
+        <label>Change checkbox</label>
+        <input
+          type="checkbox"
+          checked={this.state.checked}
+          onChange={this.setCheckboxValue}
+        />
       </div>
     );
   }
