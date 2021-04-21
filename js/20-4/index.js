c66fa4491d385dfddc1d1c7571780c7c2157f499
YamanKatmawi <yamankatmawi@gmx.de>
2021-04-21 15:36:38 +0200

    Data type

--- js/20-4/index.js
+++ js/20-4/index.js
@@ -1,5 +1,19 @@
 //Learn javaScripts variables
 /*global,console,alert*/
+
 let myPrice = 100; // undefined
 
 console.log(myPrice);
+/////
+//Data Type
+
+//boolean (true-false)
+let mainPrice = 450;
+let hasDiscount = true;
+if (hasDiscount === true) {
+  mainPrice == 350;
+} else {
+  mainPrice = 450;
+}
+console.log(hasDiscount);
+//document.getElementById("test").innerHTML = hasDiscount;
