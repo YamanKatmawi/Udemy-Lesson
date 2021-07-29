1efd4887281c198b7cd3e908e7b35ad15b062d8d
YamanKatmawi <yamankatmawi@gmx.de>
2021-07-29 15:46:18 +0200

    loops

--- typescript/29-7/index.js
+++ typescript/29-7/index.js
@@ -1,7 +1,14 @@
-var info = {
-    name: "yaman",
-    age: 27,
-    job: "devloper"
-};
-var UserInfo = "user" + info.name + ", age:" + info.age;
-console.log(UserInfo);
+//////// template String
+// var info = {
+//   name: "yaman",
+//   age: 27,
+//   job: "devloper",
+// };
+// var UserInfo = `user${info.name}, age:${info.age}`;
+// console.log(UserInfo);
+//////////// loops
+var jobs = ["developer", "admin", "tester", "ca"];
+for (var _i = 0, jobs_1 = jobs; _i < jobs_1.length; _i++) {
+    var job = jobs_1[_i];
+    console.log("jobs title: " + job);
+}
--- typescript/29-7/index.ts
+++ typescript/29-7/index.ts
@@ -1,7 +1,14 @@
-var info = {
-  name: "yaman",
-  age: 27,
-  job: "devloper",
-};
-var UserInfo = `user${info.name}, age:${info.age}`;
-console.log(UserInfo);
+//////// template String
+// var info = {
+//   name: "yaman",
+//   age: 27,
+//   job: "devloper",
+// };
+// var UserInfo = `user${info.name}, age:${info.age}`;
+// console.log(UserInfo);
+
+//////////// loops
+let jobs = ["developer", "admin", "tester", "ca"];
+for (var job of jobs) {
+  console.log(`jobs title: ${job}`);
+}
