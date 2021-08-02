0cedad824b3f236356f8c6ab1da1f0ab090a720f
YamanKatmawi <yamankatmawi@gmx.de>
2021-08-02 13:40:28 +0200

    Functions

--- typescript/2-8/index.js
+++ typescript/2-8/index.js
@@ -1,15 +1,24 @@
-var Name;
-Name = "yaman";
-var age;
-age = 27;
-var married;
-married = true;
-var addation;
-addation.push("job developer");
-addation.push(321456989);
-////////////
-var info = {
-    Name1: "Ahmad",
-    age: 28,
-    married: true
-};
+// var Name: string;
+// Name = "yaman";
+// var age: number;
+// age = 27;
+// var married: boolean;
+// married = true;
+// var addation: any[];
+// addation.push("job developer");
+// addation.push(321456989);
+// ////////////
+// var info = {
+//   Name1: "Ahmad",
+//   age: 28,
+//   married: true,
+//   getjobs: function () {
+//     console.log("he is developer");
+//   },
+// };
+// info.getjobs();
+//////////////// Function////////////
+function info(name, age) {
+    return "user name:" + name + ",age:" + age;
+}
+info("yaman", 789);
