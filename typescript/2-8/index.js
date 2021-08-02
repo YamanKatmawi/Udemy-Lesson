d040d6ca6b690c1422dc0d80147e344e4564b365
YamanKatmawi <yamankatmawi@gmx.de>
2021-08-02 12:33:00 +0200

    Variable Types

---
+++ typescript/2-8/index.js
@@ -0,0 +1,15 @@
+var Name;
+Name = "yaman";
+var age;
+age = 27;
+var married;
+married = true;
+var addation;
+addation.push("job developer");
+addation.push(321456989);
+////////////
+var info = {
+    Name1: "Ahmad",
+    age: 28,
+    married: true
+};
---
+++ typescript/2-8/index.ts
@@ -0,0 +1,15 @@
+var Name: string;
+Name = "yaman";
+var age: number;
+age = 27;
+var married: boolean;
+married = true;
+var addation: any[];
+addation.push("job developer");
+addation.push(321456989);
+////////////
+var info = {
+  Name1: "Ahmad",
+  age: 28,
+  married: true,
+};
